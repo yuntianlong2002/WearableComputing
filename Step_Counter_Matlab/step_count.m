eb0df099603578daf33efa55ac8d8163efb18f50
yuntianlong2002 <yuntianlong2002@gmail.com>
2016-01-18 22:01:51 -0500

    Comment the matlab script

--- Step_Counter_Matlab/step_count.m
+++ Step_Counter_Matlab/step_count.m
@@ -1,5 +1,4 @@
 close all;
-clear all;
 
 % Data input and parse to t, x, y, z axis, 
 m = csvread('pure_data.csv');
@@ -7,30 +6,35 @@ x = m(:,2);
 y = m(:,3);
 z = m(:,4);
 t = m(:,1);
+
+% Compute the magnitude and draw the original data
 mag =sqrt(x.^2+y.^2+z.^2);
 subplot(2,1,1);
 plot(t, mag);
 total_size = size(t,1);
 
-% Defination
+% Definations
 alpha = 0.1;
 smoothed = zeros(total_size,1);
 smoothed(1) = mag(1);
 window_size = 180;
 
-% Loop through the data and process
+% Loop through the data and exp smooth
 for i=2:total_size
     smoothed(i) = alpha*mag(i) + (1-alpha)*smoothed(i-1);
 end
 
+% Plot the smoothed data
 subplot(2,1,2);
 plot(t, smoothed);
 hold on;
+
 
 for j=1:window_size:total_size
     if j+window_size-1>total_size
         break;
     end
+    % Draw the minimum/maximum/middle value
     data_buffer = smoothed(j:j+window_size-1);
     min_val = min(data_buffer);
     max_val = max(data_buffer);
@@ -39,6 +43,7 @@ for j=1:window_size:total_size
     plot([t(j) t(j+window_size-1)], [max_val max_val],'r');
     plot([t(j) t(j+window_size-1)], [avg_val avg_val],'g');
     
+    % Find the data points that cross the middle value
     data_buffer_avg = data_buffer - avg_val;
     aboveZero = data_buffer_avg > 0;
     zeroCrossing = diff(aboveZero) == 1;
@@ -48,7 +53,8 @@ for j=1:window_size:total_size
     
 end
 % heuristic: these is lower and up limit for step speed
-
+% also: if the difference bwtween the min and max is very small,
+% then the user might not walking but doing other kind of activities
 
 
 
