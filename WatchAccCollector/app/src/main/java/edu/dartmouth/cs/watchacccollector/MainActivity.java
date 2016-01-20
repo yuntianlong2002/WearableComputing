2525e34c44b945b1aeb6e3b9507bdfa3290bc845
yuntianlong2002 <yuntianlong2002@gmail.com>
2016-01-19 21:42:08 -0500

    Update

--- WatchAccCollector/app/src/main/java/edu/dartmouth/cs/watchacccollector/MainActivity.java
+++ WatchAccCollector/app/src/main/java/edu/dartmouth/cs/watchacccollector/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import java.io.File;
@@ -41,6 +42,21 @@ public class MainActivity extends WearableActivity implements SensorEventListene
      * Step count to be displayed in UI
      */
     private int stepCount = 0;
+    private double calCount = 0;
+    private double distCount = 0;
+    private int height = 175;
+    private int weight = 60;
+
+
+    // Tuple class
+    public class Tuple<X, Y> {
+        public final X timestamp;
+        public final Y value;
+        public Tuple(X t, Y v) {
+            this.timestamp = t;
+            this.value = v;
+        }
+    }
 
     /**
      * Is accelerometer running?
@@ -55,9 +71,16 @@ public class MainActivity extends WearableActivity implements SensorEventListene
 	 * Various UI components
 	 */
     private TextView stepsView;
+    private TextView calView;
+    private TextView distView;
+    private SeekBar seekBar_height;
+    private SeekBar seekBar_weight;
+    private TextView heightPrint;
+    private TextView weightPrint;
+
     private CompoundButton accelButton;
 
-    private static ArrayBlockingQueue<Double> mAccBuffer;
+    private static ArrayBlockingQueue<Tuple<Long, Double> > mAccBuffer;
     int ACCELEROMETER_BLOCK_CAPACITY = 160; // Size of processing window
     int ACCELEROMETER_BUFFER_CAPACITY = 3200; // Size of the blocking buffer
     // Heuristic: these is lower and up limit for step speed
@@ -76,7 +99,7 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         setContentView(R.layout.activity_main);
         setAmbientEnabled();
 
-        mAccBuffer = new ArrayBlockingQueue<Double>(ACCELEROMETER_BUFFER_CAPACITY);
+        mAccBuffer = new ArrayBlockingQueue<>(ACCELEROMETER_BUFFER_CAPACITY);
 
         mContainerView = (BoxInsetLayout) findViewById(R.id.container);
 
@@ -87,6 +110,58 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         mAsyncTask.execute();
 
         stepsView = (TextView)findViewById(R.id.stepCount);
+        calView = (TextView)findViewById(R.id.calCount);
+        distView = (TextView)findViewById(R.id.distCount);
+        seekBar_height = (SeekBar) findViewById(R.id.height);
+        heightPrint = (TextView)findViewById(R.id.heightInput);
+        height = seekBar_height.getProgress() + 130;
+        heightPrint.setText(height + "cm");
+
+        seekBar_weight = (SeekBar) findViewById(R.id.weight);
+        weightPrint = (TextView)findViewById(R.id.weightInput);
+        weight = seekBar_weight.getProgress() + 50;
+        weightPrint.setText(weight + "kg");
+
+
+        seekBar_height.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                height = seekBar.getProgress() + 130;
+                heightPrint.setText(height + "cm");
+            }
+        });
+
+        seekBar_weight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                weight = seekBar.getProgress() + 50;
+                weightPrint.setText(weight + "kg");
+            }
+        });
+
+
         //Set the buttons and the text accordingly
         accelButton = (ToggleButton) findViewById(R.id.StartButton);
         accelButton.setChecked(isAccelRunning);
@@ -154,6 +229,8 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         double SMOOTH_FACTOR = 0.1;
         filter = new Filter(SMOOTH_FACTOR);
         stepCount = 0;
+        calCount = 0;
+        distCount = 0;
     }
 
     /**
@@ -179,18 +256,18 @@ public class MainActivity extends WearableActivity implements SensorEventListene
 
             // Use blocking queue to reduce letency
             try {
-                mAccBuffer.add(new Double(smoothed));
+                mAccBuffer.add(new Tuple(System.currentTimeMillis(), smoothed));
             } catch (IllegalStateException e) {
 
                 // Exception happens when reach the capacity.
                 // Doubling the buffer. ListBlockingQueue has no such issue,
                 // But generally has worse performance
-                ArrayBlockingQueue<Double> newBuf = new ArrayBlockingQueue<Double>(
+                ArrayBlockingQueue<Tuple<Long,Double>> newBuf = new ArrayBlockingQueue<>(
                         mAccBuffer.size() * 2);
 
                 mAccBuffer.drainTo(newBuf);
                 mAccBuffer = newBuf;
-                mAccBuffer.add(new Double(smoothed));
+                mAccBuffer.add(new Tuple(System.currentTimeMillis(), smoothed));
             }
 
         }
@@ -207,6 +284,9 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             double max = Double.MIN_VALUE;
             double min = Double.MAX_VALUE;
 
+            long start_timestamp = 0;
+            long end_timestamp = 0;
+
             while (true) {
                 try {
                     // need to check if the AsyncTask is cancelled or not in the while loop
@@ -216,9 +296,19 @@ public class MainActivity extends WearableActivity implements SensorEventListene
                     }
 
                     // Dumping buffer
-                    accBlock[blockSize++] = mAccBuffer.take().doubleValue();
+                    Tuple<Long,Double> curr = mAccBuffer.take();
+                    if(blockSize==0){
+                        start_timestamp = curr.timestamp.longValue();
+                    }
+                    accBlock[blockSize++] = curr.value.doubleValue();
+
+
 
                     if (blockSize == ACCELEROMETER_BLOCK_CAPACITY) {
+                        end_timestamp = curr.timestamp.longValue();
+                        long time_diff_milli = end_timestamp - start_timestamp;
+
+
                         blockSize = 0;
 
                         // Find the minimum/maximum/middle value of current window
@@ -234,21 +324,62 @@ public class MainActivity extends WearableActivity implements SensorEventListene
                             }
                         }
 
-                        // Determine whether the user is walking through this threshold
-                        if(max - min<STILLNESS_THRESHOLD)
-                            continue;
 
                         double avg = (max+min)/2.0;
                         int last_zero_idx = -ACCELEROMETER_BLOCK_CAPACITY;
 
-                        for(int i=1;i<ACCELEROMETER_BLOCK_CAPACITY;i++){
+                        int steps_this_window = 0;
 
-                            // The user's walking speed is withing a range
-                            if((accBlock[i]-avg)*(accBlock[i-1]-avg)<0 && i-last_zero_idx>MINIMUM_STEP_TIME_DIFF) {
-                                stepCount++;
-                                last_zero_idx = i;
+                        // Determine whether the user is walking through this threshold
+                        if(max - min>STILLNESS_THRESHOLD){
+                            for (int i = 1; i < ACCELEROMETER_BLOCK_CAPACITY; i++) {
+
+                                // The user's walking speed is withing a range
+                                if ((accBlock[i] - avg) * (accBlock[i - 1] - avg) < 0 && i - last_zero_idx > MINIMUM_STEP_TIME_DIFF) {
+                                    steps_this_window++;
+                                    last_zero_idx = i;
+                                }
                             }
                         }
+
+                        // Compute distance
+                        if(steps_this_window>0){
+                            double steps_per_two_seconds = 2*steps_this_window/(time_diff_milli/1000.0);
+                            double stride_length;
+                            switch((int)steps_per_two_seconds){
+                                case 1:
+                                    stride_length = (double)height/500.0;
+                                    break;
+                                case 2:
+                                    stride_length = (double)height/400.0;
+                                    break;
+                                case 3:
+                                    stride_length = (double)height/300.0;
+                                    break;
+                                case 4:
+                                    stride_length = (double)height/200.0;
+                                    break;
+                                case 5:
+                                    stride_length = (double)height/100.0/1.2;
+                                    break;
+                                case 6:
+                                    stride_length = (double)height/100.0;
+                                    break;
+                                default:
+                                    stride_length = (double)height/100.0*1.2;
+                            }
+                            double speed = steps_per_two_seconds*stride_length/2.0;
+                            distCount = distCount + (speed * (time_diff_milli/1000.0));
+
+                            double cal_per_one_sec = speed * weight/800.0;
+                            calCount = calCount + (cal_per_one_sec*(time_diff_milli/1000.0));
+
+
+                        } else {
+                            calCount = calCount + (1.0 * weight/1800.0 *(time_diff_milli/1000.0) / 2);
+                        }
+
+                        stepCount+=steps_this_window;
 
                         publishProgress();
                     }
@@ -280,5 +411,7 @@ public class MainActivity extends WearableActivity implements SensorEventListene
 
     private void sendUpdatedStepCountToUI() {
         stepsView.setText("Steps=" + stepCount);
+        calView.setText("Cal=" + (int)calCount);
+        distView.setText("Dist=" + (int)distCount + " m");
     }
 }
